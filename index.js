daf8a05b5d8706d3a048b20831cdd5a3d39816d2
Saurav kumar <39781340+sauravkumararya@users.noreply.github.com>
2019-05-21 02:52:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-
-const ipp = require('instagram-profile-picture');
-
-var https = require('https');
-
-
-https.createServer(function (req, res) {/*
-  console.log(req.url);
-  var u = req.url.replace(/[/]/g , '');
-  console.log(u) 
-ipp(u).then(user => {
+// using the http module
+let http = require('http'),
  
-  console.log(req.url);
-  res.writeHead(200,{'Content-Type':'text/html'})
-console.log(user);
-res.write('<img src="'+user+'"/>')
-res.end(); 
-
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
  
-
+// create a simple server
+let server = http.createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
 });
-    
-*/
-
-res.write('hello')
-res.end(); 
-}).listen(3000);
-
