dadf7738b06471fe943a3c8c559c116ba0b815fa
Saurav kumar <39781340+sauravkumararya@users.noreply.github.com>
2019-05-21 03:03:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // using the http module
-let http = require('http'),
- 
+let http = require('http');
+const ipp = require('instagram-profile-picture');
 // look for PORT environment variable, 
 // else look for CLI argument,
 // else use hard coded value for port 8080
@@ -8,12 +8,29 @@ port = process.env.PORT || process.argv[2] || 8080;
  
 // create a simple server
 let server = http.createServer(function (req, res) {
+
  
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.write('hello heroku!', 'utf-8');
-        res.end();
+ 
+        console.log(req.url);
+  var u = req.url.replace(/[/]/g , '');
+  console.log(u) 
+ipp(u).then(user => {
+ 
+  console.log(req.url);
+  res.writeHead(200,{'Content-Type':'text/html'})
+console.log(user);
+res.write('<img src="'+user+'"/>')
+
+
+res.write('Crafted By saurav', 'utf-8');
+res.end();
+
+});
+    
+
+ 
+ 
+    
  
     });
  
